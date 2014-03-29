1ec7e9df32370a2d4745e83a9d170bf1a949f2c9
Joel Denke <mail@happyness.se>
2014-03-29 19:26:59 +0100

    Multiple graphs in one supported

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function generateData(data)
 
 function generateGraph(title, legends, data, bodyId, divId)
 {
-    var w = 500,
-        h = 500;
+    var w = 300,
+        h = 300;
 
     var colorscale = d3.scale.category10();
     var d = generateData(data);
@@ -112,28 +112,85 @@ function generate()
 {
     console.log("Start generate chart");
 
-    var dataElements = document.getElementsByName("data[]");
-    var tagElements  = document.getElementsByName("tag[]");
-    var data = [];
+    var noForms = parseInt(document.getElementById("numberOfForms").value);
+    var graphs = [];
+    var labels = [];
+    var title = document.getElementById("title").value;
 
-    for (i=0; i < dataElements.length; i++) {
-        data.push({data: parseFloat(dataElements[i].value), label: tagElements[i].value});
+    for (var i = 1; i <= noForms; i++) {
+        var form         = document.getElementById("formData"+i);
+        var dataElements = form.querySelectorAll("input[name='data[]']");
+        var tagElements  = form.querySelectorAll("input[name='tag[]']");
+        var data = [];
+        var value;
+
+        console.log(dataElements);
+
+        for (var j = 0; j < dataElements.length; j++) {
+            value = parseFloat(dataElements[j].value);
+
+            console.log(dataElements[j]);
+
+
+            if (!isNaN(value)) data.push({data: value, label: tagElements[j].value});
+        }
+
+        var typeLabel = document.getElementById("chartType" + i).value;
+
+        console.log(data);
+
+        if (data.length > 0) {
+            labels.push(typeLabel);
+            graphs.push(data);
+        }
     }
 
-    var title = document.getElementById("chartTitle").value;
-    var typeLabel = document.getElementById("chartType").value;
-
-    generateGraph(title, [typeLabel], [data], '#body', '#chart');
+    if (graphs.length > 0) {
+        generateGraph(title, labels, graphs, '#body', '#chart');
+    }
 }
 
-function addField()
+function addField(fields)
 {
-    var element = '<p>' +
-        '<label for="tag[]">Tag</label>' +
-        '<input id="tag[]" name="tag[]" type="text" />' +
-        '<input id="data[]" name="data[]" type="text" />' +
-        ' <label for="data[]">Data</label>' +
-        '</p>';
+    fields.insertAdjacentHTML("beforeend", getFields(1));
+}
 
-    document.getElementById("fields").insertAdjacentHTML("beforeend", element);
+function getFields(noFields)
+{
+    var fields = '';
+
+    for (var i = 1; i <= noFields; i++) {
+        fields  += '<p>' +
+            '<label for="tag[]">Tag</label>' +
+            '<input id="tag[]" name="tag[]" type="text" />' +
+            ' <label for="data[]">Data</label>' +
+            '<input id="data[]" name="data[]" type="text" />' +
+            '</p>';
+    }
+
+    return fields;
+}
+
+function addForms()
+{
+    var noForms = parseInt(document.getElementById("numberOfForms").value);
+    var noFields = parseInt(document.getElementById("numberOfFields").value);
+    var dataForms = document.getElementById("dataForms");
+
+    console.log(noForms);
+    console.log(noFields);
+
+    for (var i = 1; i <= noForms; i++)
+    {
+        dataForms.insertAdjacentHTML("beforeend", generateForm(i, noFields));
+    }
+}
+
+function generateForm(number, noFields)
+{
+    return '<form id="formData'+ number+'" class="formData">' +
+        '<p><label for="chartType'+number+'">Chart title</label><input id="chartType'+number+'"/></p>' +
+        '<p id="fields'+number+'">'+ getFields(noFields) +'</p>' +
+        '<p><a href="#" onClick="addField(' + "fields" + number + ')">Lägg till flera fält</a></p>' +
+        '</form>';
 }
